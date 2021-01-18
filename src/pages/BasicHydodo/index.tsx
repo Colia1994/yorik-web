8f1e22917dce8e4f7efc6c757b08b10a98323b9c
colia <colia@foxmail.com>
2021-01-18 17:38:25 +0800

    all push

---
+++ src/pages/BasicHydodo/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { ResponsiveGrid } from '@alifd/next';
+import PageHeader from '@/components/PageHeader';
+import BasicList from './components/BasicList';
+
+const { Cell } = ResponsiveGrid;
+
+const BasicHydodo = () => {
+  return (
+    <ResponsiveGrid gap={20}>
+      <Cell colSpan={12}>
+        <PageHeader
+          title="商品查询"
+          breadcrumbs={[{ name: '慧多多' }, { name: '商品查询' }]}
+          description="模拟查询，查询佣金等完整参数"
+        />
+      </Cell>
+
+      <Cell colSpan={12}>
+        <BasicList />
+      </Cell>
+    </ResponsiveGrid>
+  );
+};
+
+export default BasicHydodo;
